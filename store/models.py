e602e3d10cd1a21b5cc7b065a4a6e6017f3ebe41
Manoharan-R <manoharanrajan007@gmail.com>
2024-03-24 15:12:10 +0530

    Store and carts functionality

--- store/models.py
+++ store/models.py
@@ -21,3 +21,25 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+class VariationManager(models.Manager):
+    def usages(self):
+        return super(VariationManager, self).filter(variation_category='usage', is_active=True)
+    def delivery_type(self):
+        return super(VariationManager, self).filter(variation_category='delivery', is_active=True)
+
+variation_category_choice = (
+    ('usage', 'usage'),
+    ('delivery', 'delivery'),
+)
+class Variation(models.Model):
+    product  = models.ForeignKey(Product, on_delete =models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects= VariationManager()
+    def __str__(self):
+        return self.variation_value
